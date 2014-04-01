fd9f38b599b0c770795aa8e13a3c15a01fe31e2a
Yan Chen <yanchen036@gmail.com>
2014-04-01 23:34:23 +0800

    coding Node class

--- pylearning/tree/decision_tree.py
+++ pylearning/tree/decision_tree.py
@@ -1,15 +1,53 @@
 # -*- coding: utf-8 -*-
 # yanchen036@gmail.com
 
-from ..util.dataset import Sample, Dataset
+from ..util.dataset import Instance, Dataset
 
 class Node():
     def __init__(self):
-        lch = None
-        rch = None
-        sample_idx = None
-        split_fea = None
-        split_val = None
+        self.children = None
+        self.children_split_val = None
+        self.data_list = []
+        self.split_fea = None
+        self._label = None
+
+    def split(self, dataset):
+        assert self.data_list != None
+        for feaidx in range(0, self.dataset.get_fea_num()):
+            # calc info gain
+            pass
+
+        self.children = []
+        self.children_split_val = []
+        for idx in self.data_list:
+            ins = dataset.get_ins(idx)
+            if ins.fea[self.split_fea] in self.children_feaval:
+                chidx = self.children_split_val.index(ins.fea[self.split_fea])
+                self.children[chidx].data_list.append(idx)
+            else:
+                self.children.append(Node())
+                self.children[-1].data_list.append(idx)
+
+    def get_label(self, dataset):
+        if self._label is None:
+            label_dict = {}
+            max_cnt = 0
+            max_label = None
+            for idx in self.data_list:
+                ins = dataset.get_ins(idx)
+                if ins.label in label_dict.keys():
+                    label_dict[ins.label] += 1
+                else:
+                    label_dict[ins.label] = 1
+            for l, cnt in label_dict.iteritems():
+                if cnt > max_cnt:
+                    max_cnt = cnt
+                    max_label = l
+            self._label = max_label
+        return self._label
+
+
+
 
 class DTree():
     def __init__(self):
--- pylearning/util/dataset.py
+++ pylearning/util/dataset.py
@@ -1,20 +1,29 @@
 # -*- coding: utf-8 -*-
 # yanchen036@gmail.com
 
-class Sample():
+class Instance():
     def __init__(self, lable=0, fea=[]):
         self.label = lable
         self.fea = fea
 
 class Dataset():
     def __init__(self):
-        _samples = []
+        self._data = []
+        # key: fea_idx, value: list of fea_val
+        self._feaset = {}
 
-    def insert(self, s):
-        self.samples.append(s)
+    def load(self, file):
+        pass
 
-    def get(self, idx):
-        return self.samples[idx]
+    def get_ins(self, idx):
+        return self._data[idx]
 
-    def getsize(self):
-        return len(self.samples)
+    def get_ins_len(self):
+        return len(self._data)
+
+    def get_spec_fea_val_domain(self, feaidx):
+        return self._feaset[feaidx]
+
+    def get_fea_num(self):
+        assert self.data is not None and len(self.data) > 0
+        return len(self.data[0].fea)
